51ecb26a29b0531357f4668d498646f926a983ad
Maxim <neversayhello@mail.ru>
2017-05-05 16:53:57 +0300

    5/5/2017

--- PrintStore/Models/AccountViewModels.cs
+++ PrintStore/Models/AccountViewModels.cs
@@ -69,6 +69,8 @@ namespace PrintStore.Models
         [Display(Name = "Email")]
         public string Email { get; set; }
 
+        public string UserRoles { get; set; }
+
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
